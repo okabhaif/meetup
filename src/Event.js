05d299eabfb4ddba7facb6ca1662324a0930f39d
okabhaif <okabhaif@users.noreply.github.com>
2020-06-08 19:07:58 +0100

    Feat: 4.3 - unit testing
    added tests to try out some mock event data.

--- src/Event.js
+++ src/Event.js
@@ -3,6 +3,23 @@ import React, { Component } from 'react';
 class Event extends Component {
 
   state = {
+    event:
+    {
+      local_time: '19:00',
+      link: 'https://www.meetup.com/Lichfield-Ladies-over-40s-Book-Club-with-wine-and-friends/events/269039500/',
+      name: 'Lichfield Ladies over 40s Book Club with wine and friends',
+      id: '32534983',
+      join_mode: 'approval',
+      urlname: 'Lichfield-Ladies-over-40s-Book-Club-with-wine-and-friends',
+      who: 'Members',
+      localized_location: 'Lichfield, United Kingdom',
+      country: 'gb',
+      timezone: 'Europe/London',
+      waitlist_count: 0,
+      yes_rsvp_count: 7,
+      description: '1984 by George Orwell'
+    }
+    ,
     showDetails: false
   }
 
@@ -20,12 +37,18 @@ class Event extends Component {
     let showDetails = this.state.showDetails;
 
     return (
+      <div className="event-container">
+        <div className="event-details">
+          <p className="event-name">{this.state.event.name}</p>
+          <p className="event-location">{this.state.event.localized_location}</p>
+          {!this.state.showDetails && <button onClick={this.handleShowDetails} className="showDetails"> Details </button>}
+        </div>
 
-      <div className="event-details">
-        {!this.state.showDetails && <button onClick={this.handleShowDetails} className="showDetails"> Details </button>}
 
         {showDetails && <div className="show-details">
-          <p className="event-description"></p>
+
+          <p className="event-description">{this.state.event.description}</p>
+          <p className="event-link">{this.state.event.link}</p>
           <button onClick={this.handleToggleDetails} className="hideDetails"> Hide Details </button>
         </div>}
 
--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -26,6 +26,8 @@ describe('<Event /> component', () => {
     expect(EventWrapper.state('showDetails')).toBe(true);
   });
 
+
+
   test('click on button should hide details', () => {
     EventWrapper.setState({
       showDetails: true
@@ -46,6 +48,28 @@ describe('<Event /> component', () => {
       showDetails: false
     });
     expect(EventWrapper.find('.event-description')).toHaveLength(0);
+  });
+
+  test('find event title from event object', () => {
+    expect(EventWrapper.state('event')).toHaveProperty('name', 'Lichfield Ladies over 40s Book Club with wine and friends');
+  });
+
+  test('event title', () => {
+    console.log(EventWrapper.state('event'));
+
+    expect(EventWrapper.state('event').name).toBe('Lichfield Ladies over 40s Book Club with wine and friends');
+  });
+
+
+  test('render event title', () => {
+    expect(EventWrapper.find('.event-name').first().text()).toEqual('Lichfield Ladies over 40s Book Club with wine and friends');
+  });
+
+  test('render description when showDetails===true', () => {
+    EventWrapper.setState({
+      showDetails: true
+    });
+    expect(EventWrapper.find('.event-description').first().text()).toEqual('1984 by George Orwell');
   });
 
 });
