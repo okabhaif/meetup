df0a21c3494f1ea836b79db72451bef6c077a0ae
okabhaif <okabhaif@users.noreply.github.com>
2020-06-08 19:12:09 +0100

    fix: refactored tests to include mock data

--- src/Event.js
+++ src/Event.js
@@ -3,23 +3,7 @@ import React, { Component } from 'react';
 class Event extends Component {
 
   state = {
-    event:
-    {
-      local_time: '19:00',
-      link: 'https://www.meetup.com/Lichfield-Ladies-over-40s-Book-Club-with-wine-and-friends/events/269039500/',
-      name: 'Lichfield Ladies over 40s Book Club with wine and friends',
-      id: '32534983',
-      join_mode: 'approval',
-      urlname: 'Lichfield-Ladies-over-40s-Book-Club-with-wine-and-friends',
-      who: 'Members',
-      localized_location: 'Lichfield, United Kingdom',
-      country: 'gb',
-      timezone: 'Europe/London',
-      waitlist_count: 0,
-      yes_rsvp_count: 7,
-      description: '1984 by George Orwell'
-    }
-    ,
+    event: {},
     showDetails: false
   }
 
--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -50,23 +50,44 @@ describe('<Event /> component', () => {
     expect(EventWrapper.find('.event-description')).toHaveLength(0);
   });
 
-  test('find event title from event object', () => {
-    expect(EventWrapper.state('event')).toHaveProperty('name', 'Lichfield Ladies over 40s Book Club with wine and friends');
-  });
-
-  test('event title', () => {
-    console.log(EventWrapper.state('event'));
-
-    expect(EventWrapper.state('event').name).toBe('Lichfield Ladies over 40s Book Club with wine and friends');
-  });
-
-
   test('render event title', () => {
+    EventWrapper.setState({
+      event: {
+        local_time: '19:00',
+        link: 'https://www.meetup.com/Lichfield-Ladies-over-40s-Book-Club-with-wine-and-friends/events/269039500/',
+        name: 'Lichfield Ladies over 40s Book Club with wine and friends',
+        id: '32534983',
+        join_mode: 'approval',
+        urlname: 'Lichfield-Ladies-over-40s-Book-Club-with-wine-and-friends',
+        who: 'Members',
+        localized_location: 'Lichfield, United Kingdom',
+        country: 'gb',
+        timezone: 'Europe/London',
+        waitlist_count: 0,
+        yes_rsvp_count: 7,
+        description: '1984 by George Orwell'
+      }
+    });
     expect(EventWrapper.find('.event-name').first().text()).toEqual('Lichfield Ladies over 40s Book Club with wine and friends');
   });
 
   test('render description when showDetails===true', () => {
     EventWrapper.setState({
+      event: {
+        local_time: '19:00',
+        link: 'https://www.meetup.com/Lichfield-Ladies-over-40s-Book-Club-with-wine-and-friends/events/269039500/',
+        name: 'Lichfield Ladies over 40s Book Club with wine and friends',
+        id: '32534983',
+        join_mode: 'approval',
+        urlname: 'Lichfield-Ladies-over-40s-Book-Club-with-wine-and-friends',
+        who: 'Members',
+        localized_location: 'Lichfield, United Kingdom',
+        country: 'gb',
+        timezone: 'Europe/London',
+        waitlist_count: 0,
+        yes_rsvp_count: 7,
+        description: '1984 by George Orwell'
+      },
       showDetails: true
     });
     expect(EventWrapper.find('.event-description').first().text()).toEqual('1984 by George Orwell');
