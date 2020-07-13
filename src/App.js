7091a65024ee95ba46d150b609bffd0ba42d9a15
okabhaif <okabhaif@users.noreply.github.com>
2020-07-13 16:54:31 +0100

    feat: added scatterchart - 4.9.
    (course content exercise)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 import './App.css';
 import EventList from './EventList';
@@ -54,12 +56,52 @@ class App extends Component {
     return debouncedGetEvents(lat, lon, this.state.eventsPerPage).then(events => this.setState({ events }));
   };
 
+  countEventsOnADate = (date) => {
+    let count = 0;
+    for (let i = 0; i < this.state.events.length; i += 1) {
+      if (this.state.events[i].local_date === date) {
+        count += 1;
+      }
+    }
+    return count;
+  }
+  getData = () => {
+    const next7Days = []; // Create empty array for the next 7 days
+    const currentDate = moment(); // Today
+    // Loop 7 times for next 7 days
+    for (let i = 0; i < 7; i += 1) {
+      currentDate.add(1, 'days'); // Add 1 day to current date, currentDate changes
+      const dateString = currentDate.format('YYYY-MM-DD'); // Format the date
+      // Use the countEventsOnADate function to count #events on this date
+      const count = this.countEventsOnADate(dateString);
+      next7Days.push({ date: dateString, number: count }); // Add this date and number to the list
+    }
+    return next7Days;
+  }
+
   render() {
     return (
       <div className="App">
         <WarningAlert text={this.state.warningText} />
         <CitySearch updateEvents={this.updateEvents} />
         <NumberOfEvents eventsPerPage={this.state.eventsPerPage} handleNumberInputChanged={(value) => this.updateNumberOfEvents(value)} />
+        <ResponsiveContainer
+          height={400}>
+
+          <ScatterChart
+            margin={{
+              top: 20, right: 20, bottom: 20, left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="date" name="date" />
+            <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false}
+            />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter name="A school" data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+
         <EventList events={this.state.events} />
 
       </div>
